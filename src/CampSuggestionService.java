05c5f1127ebf4f72c99212f95daaf8085f7ab2c2
Alan Wang <alanwang@Apples-MacBook-Pro.local>
2023-11-01 14:43:43 +0800

    suggestion service

---
+++ src/CampSuggestionService.java
@@ -0,0 +1,29 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CampSuggestionService {
+    private CampRepository campRepository;
+    private List<Suggestion> suggestions;
+
+    public CampSuggestionService(CampRepository campRepository) {
+        this.campRepository = campRepository;
+        this.suggestions = new ArrayList<>();
+    }
+
+    public void suggest(Student student, Camp camp) {
+        Suggestion suggestion = new Suggestion(student, camp);
+        suggestions.add(suggestion);
+    }
+
+    public List<Suggestion> getSubmittedSuggestions(int campId, Student student) {
+        return suggestions.stream()
+                .filter(suggestion -> suggestion.getSender().equals(student) && suggestion.getSuggestion().getId() == campId)
+                .collect(Collectors.toList());
+    }
+
+    public List<Suggestion> getRespondedSuggestions(User user) {
+        return suggestions.stream()
+                .filter(suggestion -> suggestion.getReviewedBy() != null && suggestion.getReviewedBy().equals(user))
+                .collect(Collectors.toList());
+    }
+}
