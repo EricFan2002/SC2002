ff3ece1c4462ccc7b06a12c4932b4c4b8289c742
EricFan2002 <fty20020322@gmail.com>
2023-11-09 19:12:46 +0800

    fixed services

--- src/CampEnquireService.java
+++ src/CampEnquireService.java
@@ -1,34 +1,34 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import entity.user.Student;
-import entity.user.User;
-
-public class CampEnquireService {
-    private CampRepository campRepository;
-    private List<Enquiry> enquires;
-
-    public CampEnquireService(CampRepository campRepository) {
-        this.campRepository = campRepository;
-        this.enquires = new ArrayList<>();
-    }
-
-    public void enquiry(Student student, String question) {
-        Enquiry newEnquiry = new Enquiry(student, question);
-        enquires.add(newEnquiry);
-    }
-
-    public List<Enquiry> getSubmittedEnquires(String campId, Student student) {
-        return enquires.stream()
-                .filter(enquiry -> enquiry.getSender().equals(student) &&
-                        campRepository.get(campId) != null)
-                .collect(Collectors.toList());
-    }
-
-    public List<Enquiry> getRespondedEnquires(User user) {
-        return enquires.stream()
-                .filter(enquiry -> enquiry.getAnsweredBy() != null && enquiry.getAnsweredBy().equals(user))
-                .collect(Collectors.toList());
-    }
-}
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.stream.Collectors;
+//
+//import entity.user.Student;
+//import entity.user.User;
+//
+//public class CampEnquireService {
+//    private CampRepository campRepository;
+//    private List<Enquiry> enquires;
+//
+//    public CampEnquireService(CampRepository campRepository) {
+//        this.campRepository = campRepository;
+//        this.enquires = new ArrayList<>();
+//    }
+//
+//    public void enquiry(Student student, String question) {
+//        Enquiry newEnquiry = new Enquiry(student, question);
+//        enquires.add(newEnquiry);
+//    }
+//
+//    public List<Enquiry> getSubmittedEnquires(String campId, Student student) {
+//        return enquires.stream()
+//                .filter(enquiry -> enquiry.getSender().equals(student) &&
+//                        campRepository.get(campId) != null)
+//                .collect(Collectors.toList());
+//    }
+//
+//    public List<Enquiry> getRespondedEnquires(User user) {
+//        return enquires.stream()
+//                .filter(enquiry -> enquiry.getAnsweredBy() != null && enquiry.getAnsweredBy().equals(user))
+//                .collect(Collectors.toList());
+//    }
+//}
--- src/CampModificationService.java
+++ src/CampModificationService.java
@@ -1,21 +1,22 @@
-//this class adds/removes camps from Camp Repository
-public class CampModificationService {
-    private CampRepository campRepository;
-
-    public CampModificationService(CampRepository campRepository) {
-        this.campRepository = campRepository;
-    }
-
-    public String createCamp(Camp camp) {
-        campRepository.add(camp);
-        return camp.getId(); // Assuming the camp's ID is set prior to this call
-    }
-
-    public boolean deleteCamp(int id) {
-        return campRepository.remove(id);
-    }
-
-    public boolean editCamp(int id, Camp updatedCamp) {
-        return campRepository.update(updatedCamp);
-    }
-}
+//import entity.CampRepository;
+//import entity.camp.Camp;
+//public class CampModificationService {
+//    private CampRepository campRepository;
+//
+//    public CampModificationService(CampRepository campRepository) {
+//        this.campRepository = campRepository;
+//    }
+//
+//    public String createCamp(Camp camp) {
+//        campRepository.insert(camp);
+//        return camp.getID(); // Assuming the camp's ID is set prior to this call
+//    }
+//
+//    public boolean deleteCamp(int id) {
+////        return campRepository.remove(id);
+//    }
+//
+//    public boolean editCamp(int id, Camp updatedCamp) {
+////        return campRepository.update(updatedCamp);
+//    }
+//}
