ba4b12fdaf03f771f87ba138ea741f9baf73a383
Clayton Fernalo <clafernalo@gmail.com>
2023-11-09 13:31:10 +0800

    feat: user factory class

---
+++ src/UserFactory.java
@@ -0,0 +1,11 @@
+public class UserFactory {
+    public static User getUser(String userType, String ID, String name, String faculty) {
+        if (userType.equalsIgnoreCase("student")) {
+            return new Student(ID, name, faculty);
+        } else if (userType.equalsIgnoreCase("staff")) {
+            return new Staff(ID, name, faculty);
+        } else {
+            return null;
+        }
+    }
+}
