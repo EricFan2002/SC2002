54a4fb88cdc96cad5f37646104b2eefe9dda8336
Clayton Fernalo <clafernalo@gmail.com>
2023-11-16 18:50:17 +0800

    user static controller

--- src/controller/user/UserAccountManagementController.java
+++ src/controller/user/UserAccountManagementController.java
@@ -1,44 +1,45 @@
 package controller.user;
 
-import entity.UserRepository;
+import entity.RepositoryCollection;
 
 public class UserAccountManagementController {
-    private UserRepository userRepository;
 
-    public boolean changePassword(String username, String oldPassword, String newPassword) {
-        if (userRepository.getAll().filterByID(username).get(0).getPassword().equals(oldPassword)) {
-            userRepository.getAll().filterByID(username).get(0).setPassword(newPassword);
+    public static boolean changePassword(String username, String oldPassword, String newPassword) {
+        if (RepositoryCollection.getUserRepository().getAll().filterByID(username).get(0).getPassword()
+                .equals(oldPassword)) {
+            RepositoryCollection.getUserRepository().getAll().filterByID(username).get(0).setPassword(newPassword);
             return true;
         } else {
             return false;
         }
     }
 
-    public String forgetPassword(String username, String email) {
-        if (userRepository.getAll().filterByID(username).get(0).getEmail().equals(email)) {
-            return userRepository.getAll().filterByID(username).get(0).getPassword();
+    public static String forgetPassword(String username, String email) {
+        if (RepositoryCollection.getUserRepository().getAll().filterByID(username).get(0).getEmail().equals(email)) {
+            return RepositoryCollection.getUserRepository().getAll().filterByID(username).get(0).getPassword();
         } else {
             return null;
         }
     }
 
-    public boolean verifyPassword(String username, String password) {
-        if (userRepository.getAll().filterByID(username).get(0).getPassword().equals(password)) {
+    public static boolean verifyPassword(String username, String password) {
+        if (RepositoryCollection.getUserRepository().getAll().filterByID(username).get(0).getPassword()
+                .equals(password)) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean verifyEmail(String username, String email) {
-        if (userRepository.getAll().filterByID(username).get(0).getEmail().equals(email)) {
+    public static boolean verifyEmail(String username, String email) {
+        if (RepositoryCollection.getUserRepository().getAll().filterByID(username).get(0).getEmail().equals(email)) {
             return true;
         } else {
             return false;
         }
     }
 
-    public boolean AccountSecurity() {
+    public static boolean AccountSecurity() {
         return true;
     }
 }
