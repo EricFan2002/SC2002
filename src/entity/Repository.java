3f22d5880e82dd57048d68f8dd07ed4603bf2398
Clayton Fernalo <clafernalo@gmail.com>
2023-11-16 15:04:07 +0800

    return bool remove

--- src/entity/Repository.java
+++ src/entity/Repository.java
@@ -6,7 +6,6 @@ public abstract class Repository<T extends ITaggedItem> {
     protected String filePath;
     protected RepositoryList<T> all;
 
-    //constructor
     public Repository(String filePath) {
         this.filePath = filePath;
         this.all = new RepositoryList<T>();
@@ -17,10 +16,9 @@ public abstract class Repository<T extends ITaggedItem> {
     public abstract boolean load();
 
     public int size() {
-        return all.size(); 
+        return all.size();
     }
 
-    //getter
     public RepositoryList<T> getAll() {
         // deep copy
         RepositoryList<T> all = new RepositoryList<T>();
@@ -30,31 +28,26 @@ public abstract class Repository<T extends ITaggedItem> {
         return all;
     }
 
-    //add 1 item to RepositoryList 
     public void insert(T item) {
         all.add(item);
     }
 
-    //add multiple items to RepositoryList from an array
     public void insert(T[] items) {
         for (T item : items) {
             all.add(item);
         }
     }
 
-    //add multiple items to RepositoryList from another RepositoryList
     public void insert(RepositoryList<T> items) {
         for (T item : items) {
             all.add(item);
         }
     }
 
-    //remove 1 item from RepositoryList
     public boolean remove(T item) {
         return all.remove(item);
     }
 
-    //go to RepositoryList then to List and find item with the same ID as the input item and replace it with the input item
     public boolean update(T object) {
         for (T item : all) {
             if (item.getID().equals(object.getID())) {
@@ -66,7 +59,7 @@ public abstract class Repository<T extends ITaggedItem> {
         return false;
     }
 
-    //setter
+    // dangerous
     protected boolean setAll(RepositoryList<T> all) {
         this.all = all;
         return true;
